119e2b9d8b5f64b48e3f79767559a627164090fb
Yi Jiang <yi.jiang@defence.gov.au>
2022-11-19 20:35:51 +1100

    Quick fix mobile menu; remove overloaded JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log("body", document.body.clientWidth);
+
+  $(".mobile.menu").click(function () {
+    $(".mobile.menu > i").toggleClass( "fa-bar fa-times");
+    $(".mobile.menu").toggleClass( "menu-open" );
+    $(".off-canvas-menu").toggleClass( "off-canvas-menu-on");
+  });
+
+
+  $( ".off-canvas-menu > * > .link" ).click(function () {
+    $(".off-canvas-menu").toggleClass( "off-canvas-menu-on");
+    $(".mobile.menu").toggleClass( "menu-open" );
+    $(".mobile.menu > i").toggleClass( "fa-bar fa-times");
+  })
